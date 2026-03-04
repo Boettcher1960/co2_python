12524e7cbd77309c2e1bbc5da302c99e68a66a41
thomasboettcher <t.boettcher@directbox.com>
2026-03-04 10:16:38 +0100

    41f11_CO2_T.py

--- 41_CO2_T.py
+++ 41_CO2_T.py
@@ -1,4 +1,4 @@
-# 41f10_CO2_T.py 
+# 41f11_CO2_T.py 
 # Thomas Boettcher
 # part 0 variables
 # part 1 plot CO2 Mauna Loa
@@ -35,15 +35,15 @@ c7 = "red" # plot7 color
 parameter8_save_png = 8 # save png
 
 # 0.3.1 scale the left Y axis
-y_min = 350 # min value 280
-y_max = 550 # min value 440 70
+y_min = 300 # min value 280
+y_max = 650 # min value 440 70
 
 # 0.3.2 scale the right Y axis
 y_Tmin = 0 # min value °C
-y_Tmax = 4 # 4 # max value C
+y_Tmax = 7 # 4 # max value C
 
-x_anf = 1990 # 1960 geht, 2000 geht
-x_end = 2050 # 2026 geht
+x_anf = 1960 # 1960 geht, 2000 geht
+x_end = 2100 # 2026 geht
 
 ydiff = (y_max - y_min) / 10 # for y axis scale print
 xdiff = (x_end - x_anf) / 10 # for legend print
