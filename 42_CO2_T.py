dca08b89b02a02e32f8e2de04ca7f16d7450db44
thomasboettcher <t.boettcher@directbox.com>
2026-03-18 16:18:22 +0100

    v = "42c7"

--- 42_CO2_T.py
+++ 42_CO2_T.py
@@ -1,5 +1,5 @@
 # 42_CO2_T.py 
-v = "42c6" # may be bug in scaling 3.4 plot34_CO2_emission summed
+v = "42c7" # may be bug in scaling 3.4 plot34_CO2_emission summed
 # Thomas Boettcher
 # part 1 configure 
 # part 2.2 plot CO2 Mauna Loa
@@ -71,7 +71,7 @@ y_Tmin = 0 # min value °C
 y_Tmax = 5 # 4 # max value C
 
 x_anf = 1960 # 1960 2000 
-x_end = 2026 # 2200 2026 
+x_end = 2030 # 2200 2026 
 
 # 1.4.5 scale the text rows below the plot field
 tr1x = -0.09 # text row 1 x value -.3...1 -0.12
