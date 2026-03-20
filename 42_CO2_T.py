a7c72d346e46e3255685d1af1f22e99add96a5cb
thomasboettcher <t.boettcher@directbox.com>
2026-03-20 07:48:01 +0100

    #8T42 3

--- 42_CO2_T.py
+++ 42_CO2_T.py
@@ -68,15 +68,15 @@ else:
 C280=280 # CO2 concentration 1750 275 ppm
 
 # 1.3.1 scale the left Y axis
-y_min = 0 # 300 # min value 280
-y_max = 500 # 1300 # min value 440 70
+y_min = 300 # 300 # min value 280
+y_max = 350 # 1300 # min value 440 70
 
 # 1.3.2 scale the right Y axis
 y_Tmin = 0 # min value °C
 y_Tmax = 5 # 4 # max value C
 
 x_anf = 1960 # 1960 2000 -33000
-x_end = 2050 # 2200 2026 
+x_end = 1980 # 2200 2026 
 
 # 1.4.5 scale the text rows below the plot field
 tr1x = -0.09 # text row 1 x value -.3...1 -0.12
@@ -734,7 +734,7 @@ if plot34_CO2_emission > 0:
    elif plot34_CO2_emission_mode == 3:
       ax34.plot(co2_sum_world["Year"], co2_sum_world["GCumulat"], marker="o",  color=c34, label="plot34_CO2_emission")
       ax34.tick_params(axis="y", labelcolor=c34)
-      ax34.set_ylim(0, 2500) # best scaling 2000 GtCO2
+      ax34.set_ylim(300, 800) # best scaling 2000 GtCO2
    elif plot34_CO2_emission_mode == 4:
       ax34.set_ylabel("cumulative emission in GtC  34 mode 4", color=c34,
                    fontname="Arial",fontsize=20,
